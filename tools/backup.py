8e0bc9dbccd9245a9d9f5a544aab5ffbbf15a497
chubacha <154326983+chubacha@users.noreply.github.com>
2024-04-23 12:52:41 +0200

    add update messages and german language

--- tools/backup.py
+++ tools/backup.py
@@ -7,20 +7,31 @@ from time import sleep
 
 # todo: check internet conditions before attempting to download
 # initialize the web driver
+print("Initializing Firefox Web Driver...")
 driver = webdriver.Firefox()
 
 # Open the tutorials point website using get() method
+print("Opening Google Drive...")
 driver.get("https://drive.google.com/drive/folders/1Hmt9w5LzJ8E9gVRWu2hzoqX0Mgq08pZm")
 
 # clicking the courses tab in homepage.
 # driver.find_element(By.XPATH,"(//div[.= 'Fazer download de tudo'])[3]").click()
 # todo: replace the message 'Fazer download de tudo' according to the language
-WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable((By.XPATH, "(//div[.= 'Fazer download de tudo'])[3]"))).click()
+
+### Portuguese
+# WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable((By.XPATH, "(//div[.= 'Fazer download de tudo'])[3]"))).click()
+
+### German
+print("Downloading all files...")
+WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable((By.XPATH, "(//div[.= 'Alle herunterladen'])[3]"))).click()
 
 #todo: wait until download is finished. Use awaitUntil an object related to the download is displayed.
 # with this approach we would not need to wait 20 seconds
 
 # Wait 20 seconds before closing the browser
+print("Waiting 20 seconds before closing the browser...")
 sleep(20)
+print("Closing the browser...")
 driver.close()
 driver.quit()
+print("Download finished!")
