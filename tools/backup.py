334a306f7b23fba03cdcd07ece5dfed0b8542bb7
chubacha <154326983+chubacha@users.noreply.github.com>
2024-04-23 23:15:47 +0200

    minor changes to language and web driver

--- tools/backup.py
+++ tools/backup.py
@@ -11,7 +11,7 @@ import os
 # initialize the web driver
 print("Beginning download for Team Drive...")
 print("Initializing Firefox Web Driver...")
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 
 # Open the tutorials point website using get() method
 print("Opening Google Drive...")
@@ -21,12 +21,15 @@ driver.get("https://drive.google.com/drive/folders/1Hmt9w5LzJ8E9gVRWu2hzoqX0Mgq0
 # driver.find_element(By.XPATH,"(//div[.= 'Fazer download de tudo'])[3]").click()
 # todo: replace the message 'Fazer download de tudo' according to the language
 
+print("Downloading all files...")
 ### Portuguese
 # WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable((By.XPATH, "(//div[.= 'Fazer download de tudo'])[3]"))).click()
 
 ### German
-print("Downloading all files...")
-WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable((By.XPATH, "(//div[.= 'Alle herunterladen'])[3]"))).click()
+#WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable((By.XPATH, "(//div[.= 'Alle herunterladen'])[3]"))).click()
+
+### English
+WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable((By.XPATH, "(//div[.= 'Download all'])[3]"))).click()
 
 # todo: wait until download is finished. Use awaitUntil an object related to the download is displayed.
 # with this approach we would not need to wait 20 seconds
@@ -39,18 +42,21 @@ driver.close()
 driver.quit()
 
 # Move the downloaded files to the desired folder
-downloads_folder = "C:/Users/AKBSDBFSKE/Downloads/"
-backup_folder = "C:/Users/AKBSDBFSKE/Downloads/Geomundus_Backups/"
+downloads_folder = "C:/Users/Bachi/Downloads/"
+backup_folder = "C:/Users/Bachi/Downloads/Geomundus_Backups/"
 
 print("Moving downloaded files to the backup folder...")
 for file in os.listdir(downloads_folder):
-    if file.endswith(".zip"):  # Assuming the downloaded files are ZIP files, adjust as needed
+    if file.endswith("Z-001.zip"):  # Assuming the downloaded files are ZIP files, adjust as needed
         shutil.move(os.path.join(downloads_folder, file), os.path.join(backup_folder, file))
 
 print("Download finished!")
 
 
 
+
+
+'''
 # todo: check internet conditions before attempting to download
 # initialize the web driver
 print("Beginning process for Web Team Secure Files...")
@@ -65,12 +71,15 @@ driver.get("https://drive.google.com/drive/folders/14si8RQc7_oQo89el__iZKj9QylNe
 # driver.find_element(By.XPATH,"(//div[.= 'Fazer download de tudo'])[3]").click()
 # todo: replace the message 'Fazer download de tudo' according to the language
 
+print("Downloading all files...")
 ### Portuguese
 # WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable((By.XPATH, "(//div[.= 'Fazer download de tudo'])[3]"))).click()
 
 ### German
-print("Downloading all files...")
-WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable((By.XPATH, "(//div[.= 'Alle herunterladen'])[3]"))).click()
+#WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable((By.XPATH, "(//div[.= 'Alle herunterladen'])[3]"))).click()
+
+### English
+WebDriverWait(driver, 1000000).until(EC.element_to_be_clickable((By.XPATH, "(//div[.= 'Download all'])[3]"))).click()
 
 # todo: wait until download is finished. Use awaitUntil an object related to the download is displayed.
 # with this approach we would not need to wait 20 seconds
@@ -83,8 +92,8 @@ driver.close()
 driver.quit()
 
 # Move the downloaded files to the desired folder
-downloads_folder = "C:/Users/AKBSDBFSKE/Downloads/"
-backup_folder = "C:/Users/AKBSDBFSKE/Downloads/Geomundus_Backups/"
+downloads_folder = "C:/Users/Bachi/Downloads/"
+backup_folder = "C:/Users/Bachi/Downloads/Geomundus_Backups/"
 
 print("Moving downloaded files to the backup folder...")
 for file in os.listdir(downloads_folder):
@@ -94,3 +103,4 @@ for file in os.listdir(downloads_folder):
 print("Download finished!")
 
 print("Process finished!")
+'''
